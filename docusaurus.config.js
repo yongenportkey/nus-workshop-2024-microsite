30df7ca773839dc2b416d2be96e058abef66437f
Loong Yong En <loong@portkey.finance>
2023-12-29 17:35:43 +0800

    fix: edit this page link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,8 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/yongenportkey/nus-workshop-2024/",
+          editUrl:
+            "https://github.com/yongenportkey/nus-workshop-2024/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
